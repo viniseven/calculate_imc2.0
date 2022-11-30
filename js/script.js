07945abd0c3c2e6c44d56d4aa35b4b680271f1ba
Marcus Vinícius <marcusv892@gmail.com>
2022-11-29 23:23:32 -0300

    Template, script and result imc in color

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-import { calculateImc } from "./utils.js";
+import { calculateImc, showImc, showStats } from "./utils.js";
 
 const form = document.querySelector('form');
 const inputWeight = document.querySelector('#weight');
@@ -10,6 +10,38 @@ form.onsubmit = (event) => {
     const weight = Number(inputWeight.value);
     const height = Number(inputHeight.value);
 
-    console.log(calculateImc(weight, height))
+    const resultImc = calculateImc(weight, height);
+
+    const messageImc = `${resultImc}`
+
+    showImc.messageImc.innerText = messageImc;
+   
+   let messageStatusImc
+
+
+    if (resultImc < 18.5 ){
+        messageStatusImc = 'Você está abaixo do peso'
+        showImc.messageImc.style.color = '#8A0829'
+    } else if (resultImc > 18.5 && resultImc <= 24.9){
+        messageStatusImc = 'Você está com o peso normal'
+        showImc.messageImc.style.color = '#31B404'
+    }else if (resultImc >= 25 && resultImc <= 29.9){
+        messageStatusImc = 'Você está com sobrepeso'
+        showImc.messageImc.style.color = '#ffc92ecc'
+      
+    } else if(resultImc >= 30 && resultImc <= 34.9 ){
+        messageStatusImc = 'Você está com Obesidade Grau I'
+        showImc.messageImc.style.color = '#FF8000'
+    }
+     else if(resultImc >= 35 && resultImc <= 39.9){
+        messageStatusImc = 'Você está com Obesidade Grau II'
+        showImc.messageImc.style.color = '#FF4000'
+    }
+     else{
+        messageStatusImc = 'Você está com Obesidade Grau III ou Mórbida'
+        showImc.messageImc.style.color = '#B40404'
+    }
+
+    showStats.messageStats.innerText = messageStatusImc;
 }
 
--- js/utils.js
+++ js/utils.js
@@ -2,3 +2,11 @@
 export function calculateImc(weight, height){
     return Number((weight / ((height / 100) ** 2)).toFixed(1))
 }
+
+export const showImc = {
+    messageImc: document.querySelector('#resultImc')
+}
+
+export const showStats = {
+    messageStats: document.querySelector('#statusImc')
+}
